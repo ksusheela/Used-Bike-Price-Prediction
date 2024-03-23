b0bce529268e9f4ff1157802ccfc395112eec440
Susheela kothakonda <sushe9sushe@gmail.com>
2024-03-23 22:51:44 +0100

    data ingestion file added

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ HYPHEN_E_DOT = "-e ."
 def get_requirements()->List[str]:
     with open(REQUIRMENTS_FILE_NAME) as requirements_file:
         requirements_list = requirements_file.readlines()
-        requirements_list = [requirements_name.replace("\n", "") for requirements_name in rrequirements_list]
+        requirements_list = [requirements_name.replace("\n", "") for requirements_name in requirements_list]
 
     if HYPHEN_E_DOT in requirements_list:
         requirements_list.remove(HYPHEN_E_DOT)
@@ -15,9 +15,9 @@ def get_requirements()->List[str]:
     return requirements_list
 
 
-setup(name = "Regression_Project",
+setup(name = "Used_Bile_Price_prediction",
       version = "0.0.1",
-      descriptions = "MachineLearning_Used_bike__prediction",
+      descriptions = "Machine_Learning_Used_bike__prediction",
       author = "Vishala Purnima",
       authod_email = "sushe9sushe@gmail.com",
       packages = find_packages(),
